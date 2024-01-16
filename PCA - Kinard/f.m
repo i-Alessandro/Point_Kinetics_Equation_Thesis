740ef898969fb5582d0d3c6192e2312b55267954
i-Alessandro <58610791+i-Alessandro@users.noreply.github.com>
2024-01-16 22:53:33 +0100

    Delete PCA - Kinard directory

--- PCA - Kinard/f.m
+++
@@ -1,8 +0,0 @@
-function y = f(case_number, time)
-% This is the source function for our solution. It works just like rho.m
-% in that the case_number determines the function to use.
-% case_number=l : f = 0
-if case_number == 1
-    result = 0;
-end
-y=result; 
